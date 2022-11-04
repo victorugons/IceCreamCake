7c6a527a7327654fb8720ee5756e73f0b4b738df
Victor Hugo <victorugons@gmail.com>
2022-11-04 20:57:45 -0300

    feat: configure store list table view

--- IceCreamCake/StoreList/StoreListViewController+TableView.swift
+++ IceCreamCake/StoreList/StoreListViewController+TableView.swift
@@ -10,7 +10,8 @@ extension StoreListViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: StoreTableViewCell.identifier, for: indexPath) as? StoreTableViewCell else { return UITableViewCell() }
+        return cell
     }
 }
 
