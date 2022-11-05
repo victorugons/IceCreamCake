c607950ae14712d071dfa1db13e81f3186379be8
Victor Hugo <victorugons@gmail.com>
2022-11-04 21:04:19 -0300

    feat: configure banner collection view

--- IceCreamCake/StoreList/StoreListViewController+CollectionView.swift
+++ IceCreamCake/StoreList/StoreListViewController+CollectionView.swift
@@ -1,17 +1,15 @@
 import UIKit
 
 extension StoreListViewController: UICollectionViewDelegate {
-    
 }
 
 extension StoreListViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return 4
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BannerCollectionViewCell.identifier, for: indexPath) as? BannerCollectionViewCell else { return UICollectionViewCell() }
+        return cell
     }
-    
-    
 }
