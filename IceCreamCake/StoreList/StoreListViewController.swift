2054cc117f3b559619c32cc0a1b5391abfe34b98
Victor Hugo <victorugons@gmail.com>
2022-11-11 21:12:07 -0300

    feat: added page control, view customization and use of service info

--- IceCreamCake/StoreList/StoreListViewController.swift
+++ IceCreamCake/StoreList/StoreListViewController.swift
@@ -5,11 +5,12 @@ class StoreListViewController: UIViewController {
     // INCLUIR ORDENAR E FILTRO
     // INCLUIR PESQUISA
     
-    private var viewModel: StoreListViewModelProtocol
+    var viewModel: StoreListViewModelProtocol
     
     private lazy var storeTypeLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        label.text = viewModel.getStoreType()
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -17,22 +18,32 @@ class StoreListViewController: UIViewController {
     private lazy var bannersCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
-        layout.sectionInset = UIEdgeInsets(top: 2, left: 16, bottom: 2, right: 16)
+        layout.sectionInset = UIEdgeInsets(top: 2, left: 32, bottom: 2, right: 32)
         let width = UIScreen.main.bounds.width
-        layout.itemSize = CGSize(width: width - 64, height: 200)
-        layout.minimumLineSpacing = 32
+        layout.itemSize = CGSize(width: width - 96, height: 120)
+        layout.minimumLineSpacing = 64
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.delegate = self
         collectionView.dataSource = self
+        collectionView.register(BannerCollectionViewCell.self, forCellWithReuseIdentifier: BannerCollectionViewCell.identifier)
         collectionView.isPagingEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         return collectionView
+    }()
+    
+    private lazy var bannersPageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.backgroundStyle = .prominent
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        return pageControl
     }()
     
     private lazy var storeListTableView: UITableView = {
         let tableView = UITableView()
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.register(StoreTableViewCell.self, forCellReuseIdentifier: StoreTableViewCell.identifier)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     }()
@@ -48,24 +59,31 @@ class StoreListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .systemBackground
         addSubviews()
         setupConstraints()
-        
-        storeListTableView.register(StoreTableViewCell.self, forCellReuseIdentifier: StoreTableViewCell.identifier)
-        bannersCollectionView.register(BannerCollectionViewCell.self, forCellWithReuseIdentifier: BannerCollectionViewCell.identifier)
-        
-        storeTypeLabel.text = viewModel.getStoreType()
+        customizeViews()
+        viewModel.getStoreList {
+            DispatchQueue.main.async {
+                self.storeListTableView.reloadData()
+            }
+        }
+        viewModel.getBanners { [weak self] in
+            DispatchQueue.main.async {
+                self?.bannersCollectionView.reloadData()
+            }
+            self?.bannersPageControl.numberOfPages = self?.viewModel.banners.count ?? 1
+        }
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        viewModel.printStoreType()
+    func shouldChangePageControlCurrentPage(to value: Int) {
+        bannersPageControl.currentPage = value
     }
     
     private func addSubviews() {
         view.addSubview(storeTypeLabel)
         view.addSubview(bannersCollectionView)
+        view.addSubview(bannersPageControl)
         view.addSubview(storeListTableView)
     }
     
@@ -73,6 +91,7 @@ class StoreListViewController: UIViewController {
         setupStoreTypeLabelConstraints()
         setupBannersCollectionViewConstraints()
         setupStoreListTableViewConstraints()
+        setupBannersPageControlConstraints()
     }
 
     private func setupStoreTypeLabelConstraints() {
@@ -88,16 +107,37 @@ class StoreListViewController: UIViewController {
             bannersCollectionView.topAnchor.constraint(equalTo: storeTypeLabel.bottomAnchor, constant: 16),
             bannersCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             bannersCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            bannersCollectionView.heightAnchor.constraint(equalToConstant: 100)
+            bannersCollectionView.heightAnchor.constraint(equalToConstant: 124)
+        ])
+    }
+    
+    private func setupBannersPageControlConstraints() {
+        NSLayoutConstraint.activate([
+            bannersPageControl.topAnchor.constraint(equalTo: bannersCollectionView.bottomAnchor, constant: 8),
+            bannersPageControl.centerXAnchor.constraint(equalTo: bannersCollectionView.centerXAnchor)
         ])
     }
     
     private func setupStoreListTableViewConstraints() {
         NSLayoutConstraint.activate([
-            storeListTableView.topAnchor.constraint(equalTo: bannersCollectionView.bottomAnchor, constant: 16),
+            storeListTableView.topAnchor.constraint(equalTo: bannersPageControl.bottomAnchor, constant: 16),
             storeListTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             storeListTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
             storeListTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
         ])
+    }
+    
+    private func customizeViews() {
+        switch viewModel.storeType {
+        case .salgados:
+            storeTypeLabel.textColor = #colorLiteral(red: 1, green: 0.8225428462, blue: 0, alpha: 1)
+            bannersPageControl.currentPageIndicatorTintColor = #colorLiteral(red: 1, green: 0.8225428462, blue: 0, alpha: 1)
+        case .doces:
+            storeTypeLabel.textColor = #colorLiteral(red: 0.2247380912, green: 0.6563640237, blue: 0.9539867043, alpha: 1)
+            bannersPageControl.currentPageIndicatorTintColor = #colorLiteral(red: 0.2247380912, green: 0.6563640237, blue: 0.9539867043, alpha: 1)
+        case .bebidas:
+            storeTypeLabel.textColor = #colorLiteral(red: 1, green: 0, blue: 0, alpha: 0.9508226407)
+            bannersPageControl.currentPageIndicatorTintColor = #colorLiteral(red: 1, green: 0, blue: 0, alpha: 1)
+        }
     }
 }
