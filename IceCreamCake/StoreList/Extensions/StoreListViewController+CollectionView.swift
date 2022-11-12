073d2c44ec26b4ac838e9d7b684b9c76842fc761
Victor Hugo <victorugons@gmail.com>
2022-11-11 21:09:20 -0300

    feat: configure delegate and datsource functions

--- IceCreamCake/StoreList/Extensions/StoreListViewController+CollectionView.swift
+++ IceCreamCake/StoreList/Extensions/StoreListViewController+CollectionView.swift
@@ -1,15 +1,23 @@
 import UIKit
 
 extension StoreListViewController: UICollectionViewDelegate {
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let offSet = scrollView.contentOffset.x
+        let width = scrollView.frame.width
+        let horizontalCenter = width / 2
+        let pageNumber = Int(offSet + horizontalCenter) / Int(width)
+        shouldChangePageControlCurrentPage(to: pageNumber)
+    }
 }
 
 extension StoreListViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return viewModel.banners.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BannerCollectionViewCell.identifier, for: indexPath) as? BannerCollectionViewCell else { return UICollectionViewCell() }
+        cell.setupCell(with: viewModel.banners[indexPath.row])
         return cell
     }
 }
--- IceCreamCake/StoreList/Extensions/StoreListViewController+TableView.swift
+++ IceCreamCake/StoreList/Extensions/StoreListViewController+TableView.swift
@@ -6,11 +6,12 @@ extension StoreListViewController: UITableViewDelegate {
 
 extension StoreListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return viewModel.storeList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: StoreTableViewCell.identifier, for: indexPath) as? StoreTableViewCell else { return UITableViewCell() }
+        cell.setupCell(with: viewModel.storeList[indexPath.row])
         return cell
     }
 }
