afda8ac99c90d758fd3ad0d853aefc0db4afde25
Victor Hugo <victorugons@gmail.com>
2023-04-12 20:32:48 -0300

    refactor: change store service url

--- IceCreamCake/Constants.swift
+++ IceCreamCake/Constants.swift
@@ -1,4 +1,4 @@
 struct Constants {
-    public static let storeListURL: String = "https://run.mocky.io/v3/34acc88c-3b94-485c-8964-6e3a5bf8d9ba"
+    public static let storeListURL: String = "https://run.mocky.io/v3/ec33d7e2-4e89-4b71-8410-2537b28f8ff5"
     public static let bannerListURL: String = "https://run.mocky.io/v3/d3f7c8c7-2d44-4a3f-9e48-2af98e81c331"
 }
